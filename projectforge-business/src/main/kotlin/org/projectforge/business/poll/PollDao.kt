a18b42e5b288c7af4cedd157a9c7a007460800ab
NikitaMic <91180248+NikitaMic@users.noreply.github.com>
2023-07-21 14:19:33 +0200

    small changes

--- projectforge-business/src/main/kotlin/org/projectforge/business/poll/PollDao.kt
+++ projectforge-business/src/main/kotlin/org/projectforge/business/poll/PollDao.kt
@@ -52,7 +52,7 @@ open class PollDao : BaseDao<PollDO>(PollDO::class.java) {
 
         if (obj == null && operationType == OperationType.SELECT) {
             return true
-        };
+        }
         if (obj != null && operationType == OperationType.SELECT) {
             if (hasFullAccess(obj) || isAttendee(obj, ThreadLocalUserContext.user?.id!!))
                 return true
